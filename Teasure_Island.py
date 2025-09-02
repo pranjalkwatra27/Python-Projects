97c6620a1428bc91af4fa105355f02a5b9fe3a2a
pranjal kwatra <kwatrapranjal@gmail.com>
2025-09-02 20:47:50 +0530

    Create Teasure Island Project.py

---
+++ Teasure_Island.py
@@ -0,0 +1,53 @@
+print('''
+  ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _' X \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/
+''')
+print("This is the Treasure Map.")
+print("Your mission is to find the correct place where treasure is present.")
+print("You\'re currently in the middle.")
+
+want_input=input("NOW, think whether it will be in 'Right' direction or 'Left' direction?")
+if want_input == "Right" or want_input == "right" or want_input == "RIGHT":
+    print("You are just a few steps away.")
+    second_input = input("Choose the correct direction: 'Upwards' or 'downwards'.")
+    if second_input == "Upwards" or second_input == "upwards":
+        print("You are just a few steps away.")
+        print("There are 3 boxes in one of them there is treasure")
+        third_input = input("Now guess the colour of that box: brown or yellow or black")
+        if third_input == "brown" or third_input == "Brown":
+            print("Hurray!You found the treasure.")
+        else:
+            print("Oh no!Wrong Box.You lost ): Better luck next time.")
+    else:
+        print("Oh no!Wrong direction.Game over")
+else:
+    print("Oh no!Wrong direction.Game over")
+
+
+
+
+
+
